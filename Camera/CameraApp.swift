b9d8a25ae913529a38a1fac43771bcc0a3161524
Bbeniful <bbenikeszi@gmail.com>
2024-03-03 09:23:53 +0100

    Initial Commit

---
+++ Camera/CameraApp.swift
@@ -0,0 +1,17 @@
+//
+//  CameraApp.swift
+//  Camera
+//
+//  Created by Benjámin Szilágyi on 03/03/2024.
+//
+
+import SwiftUI
+
+@main
+struct CameraApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
